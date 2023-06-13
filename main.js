149962e0958a89a5991261d6393cb6ca881d7dd0
Tairê <108703534+taireg@users.noreply.github.com>
2023-06-13 12:32:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let screen1 = document.querySelector(".screen1");
 let screen2 = document.querySelector(".screen2");
 let cookie =  document.querySelector("#btnCookie");
 const btnAgain = document.querySelector("#btnAgain");
+let randomNumber = Math.round(Math.random() * 9);
+
+
 
 cookie.addEventListener("click", handleClick);
 btnAgain.addEventListener('click', handleClick);
@@ -10,7 +13,22 @@ document.addEventListener('keydown', handleEnterPress)
 
 function handleClick() {
 
+  const texts = [
+    "A vida trará coisas boas se tiver paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+    "A maior de todas as torres começa no solo.",
+    "Siga os bons e aprenda com eles.",
+    "O riso é a menor distância entre duas pessoas.",
+    "A sorte favorece a mente bem preparada.",
+    "Espere pelo mais sábio dos conselhos: o tempo.",
+    "Você é do tamanho do seu sonho.",
+  ]
 
+  screen2.querySelector('p').innerText = texts[randomNumber]
+  console.log(randomNumber)
+  textReset();
   toggleScreen();
 }
 
@@ -23,4 +41,8 @@ function handleEnterPress(e){
   if(e.key == 'Enter' && screen1.classList.contains('hide')){
     handleClick()
   }
+}
+
+function textReset(){
+  randomNumber = Math.round(Math.random() * 9);
 }
